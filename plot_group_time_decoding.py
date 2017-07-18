7e2e526ab15f69926251aaaf48c3815cca4be141
Claire Braboszcz <claire@guakamole.org>
2017-07-18 16:42:10 +0100

    time decoding

---
+++ plot_group_time_decoding.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 18 15:06:17 2017
+
+@author: claire
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+from scipy.stats import sem
+
+
+a_vs_bs = ['stim-face_vs_stim-house_allchans', 'imag-face_vs_imag-house_allchans']
+scores={'stim-face_vs_stim-house_allchans':list(),'imag-face_vs_imag-house_allchans':list()}
+
+for subject_id in [1, 2, 3, 4, 5, 6, 8, 9, 10, 11]:
+    subject = 'S%02d' %subject_id
+    data_path = os.path.join('/home/claire/DATA/Data_Face_House/' + subject +'/EEG/')
+    
+    # load score for each subject
+    for a_vs_b in a_vs_bs:
+        fname_td = os.path.join(data_path, '%s-td-auc-%s.mat' %(subject, a_vs_b))
+        mat = loadmat(fname_td)
+        scores[a_vs_b].append(mat['scores'][0])
+# average subjects
+
+times = mat['times'][0]
+mean_scores, sem_scores = dict(), dict()
+
+for a_vs_b in a_vs_bs:
+    mean_scores[a_vs_b] = np.mean(scores[a_vs_b], axis = 0)
+    sem_scores[a_vs_b] = sem(scores[a_vs_b])
+    
+    
+# Plot mean AUC across subjects
+
+colors = ['b', 'g']    
+
+for c, a_vs_b in zip(colors, a_vs_bs):
+    plt.plot(times, mean_scores[a_vs_b], c, label=a_vs_b.replace('_', ' '))
+    plt.xlabel('Time (s)')
+    plt.ylabel('Area under curve (AUC)')
+    plt.fill_between(times, mean_scores[a_vs_b] - sem_scores[a_vs_b],
+                     mean_scores[a_vs_b] + sem_scores[a_vs_b],
+                     color=c, alpha=0.2)
+plt.axhline(0.5, color='k', linestyle='--', label='Chance level')
+plt.axvline(0.0, color='k', linestyle='--')
+plt.legend()
+plt.tight_layout()
+plt.show()
+plt.savefig('time_decoding_all_elec.pdf', bbox_to_inches='tight')
+
+# plot individual subjects
+
+fig, axes = plt.subplots(5, 3, sharex=True, sharey=True, figsize=(12, 8))
+axes = axes.ravel()
+for idx in range(10):
+    axes[idx].axhline(0.5, color='k', linestyle='--', label='Chance level')
+    axes[idx].axvline(0.0, color='k', linestyle='--')
+    for a_vs_b in a_vs_bs:
+        axes[idx].plot(times, scores[a_vs_b][idx], label=a_vs_b)
+        axes[idx].set_title('sub%03d' % (idx + 1))
+
+axes[-1].axis('off')
+axes[-2].legend(bbox_to_anchor=(2.35, 0.5), loc='center right', fontsize=12)
+fig.text(0.5, 0, 'Time (s)', ha='center', fontsize=16)
+fig.text(0.01, 0.5, 'Area under curve (AUC)', va='center',
+         rotation='vertical', fontsize=16)
+plt.subplots_adjust(bottom=0.06, left=0.06, right=0.98, top=0.95)
+plt.show()
---
+++ time_decoding.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 18 10:07:21 2017
+
+@author: claire
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+
+import mne
+from mne.decoding import (SlidingEstimator, GeneralizingEstimator,
+                          cross_val_multiscore, LinearModel, get_coef)
+import os
+from mne.parallel import parallel_func
+
+
+
+def run_time_decoding(subject_id, cond1, cond2, event_id):
+    print cond1, cond2    
+    
+    subject = "S%02d" %subject_id
+    data_path = os.path.join('/home/claire/DATA/Data_Face_House/' + subject +'/EEG/')
+    
+    raw_fname = data_path + subject + '-raw.fif'
+    event_fname = data_path + subject +'-eve.fif'
+    tmin, tmax = -0.5, 1.5
+
+    raw = mne.io.read_raw_fif(raw_fname, preload=True)
+
+    events = mne.read_events(event_fname)
+
+    picks = mne.pick_types(raw.info, meg=False, eeg=True, stim=True, eog=True,
+                           exclude='bads')
+    
+    # Read epochs
+    epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=True,
+                        picks=picks, baseline=(None, 0.), preload=True,
+                        decim=4)
+    
+    epochs.pick_types(eeg=True, exclude='bads')
+
+    # only look at occipital channels
+    #select_chans = [u'Iz', u'Oz', u'O1', u'O2',u'POz', u'PO1', u'PO3', u'PO2', u'PO4']
+    #ch_names=[ch_name.replace('', '') for ch_name in select_chans]
+    #epochs.pick_types(eeg=True).pick_channels(ch_names)
+    
+    # fit and time decoder
+    X = epochs.get_data()  # MEG signals: n_epochs, n_channels, n_times
+    y = epochs.events[:, 2]  # target: Audio left or right
+
+    clf = make_pipeline(StandardScaler(), LogisticRegression())
+
+    time_decod = SlidingEstimator(clf, n_jobs=1, scoring='roc_auc')
+
+    scores = cross_val_multiscore(time_decod, X, y, cv=5, n_jobs=1)
+    # Mean scores across cross-validation splits
+    scores = np.mean(scores, axis=0)
+
+    # save scores
+    a_vs_b = '%s_vs_%s' %(cond1,cond2)
+    print 'a_vs_b = %s' %a_vs_b    
+    
+    fname_td= os.path.join(data_path, '%s-td-auc-%s_allchans.mat' %(subject, a_vs_b))
+    print 'Saving %s' %fname_td
+    from scipy.io import savemat
+    savemat(fname_td, {'scores': scores,
+                       'times':epochs.times })
+    
+    
+    
+parallel, run_func, _=parallel_func(run_time_decoding, n_jobs=6)
+parallel(run_func(subject_id, 'stim-face', 'stim-house', {'stim/face':101, 'stim/house':102})    
+        for subject_id in [1,2,3,4,5,6,8,9,10,11])
+parallel(run_func(subject_id, 'imag-face', 'imag-house', {'imag/face':201, 'imag/house':202})    
+        for subject_id in [1,2,3,4,5,6,8,9,10,11])
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
